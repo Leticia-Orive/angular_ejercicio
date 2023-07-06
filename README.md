ffb6abe0c6121af92ee022adf52c469a598f0487
Leticia Orive <leticia.orive.contractor@bbva.com>
2023-07-06 13:07:33 +0200

    3 componentes: TaskList, TaskForm y TaskDetail.

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 1.Crear un nuevo proyecto angular.
 Instalar Angular Material.
 En cada pantalla se usarán componentes de Angular Material.
+
+2.Crear 3 componentes: TaskList, TaskForm y TaskDetail.
+Crear un servicio para gestionar las tareas.
+Crear un módulo para gestionar las tareas.
+Crear un módulo para gestionar las rutas
--- task/src/app/app.module.ts
+++ task/src/app/app.module.ts
@@ -4,9 +4,12 @@ import { MatButtonModule } from '@angular/material/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TaskListComponent } from './task-list/task-list.component';
+import { TaskFormComponent } from './task-form/task-form.component';
+import { TaskDetailComponent } from './task-detail/task-detail.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, TaskListComponent, TaskFormComponent, TaskDetailComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
